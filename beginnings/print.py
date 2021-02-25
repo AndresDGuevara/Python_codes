378ed5d156b9936e4660d777633cfb391f0da361
Andres Guevara <david.784@outlook.com>
2021-02-25 18:25:34 -0500

    one more code

---
+++ beginnings/print.py
@@ -0,0 +1,14 @@
+number = 10
+string = "hello!"
+fruits = ["banana", "apple", "orange"]
+
+# Run the repl
+# then use the console and try evaluating an expression
+# using the values defined above
+
+# Examples
+# number + 5
+# string[3:]
+# len(fruits)
+print(string, number, fruits)
+print(string[3:], number + 5, len(fruits))
