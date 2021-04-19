4e8eb4944913389c5aabe8a900a889ac134f2474
Andres Guevara <david.784@outlook.com>
2021-04-19 11:21:28 -0500

    file already finishes

--- intermediate/unfinished/tuple_2lists.py
+++
@@ -1,5 +0,0 @@
-""" a tuple with two items that are both lists. 
-first one holds a list of even numbers up to 10.0 
-The second holds the odd numbers up to 20
-"""
-mytuple = ([_for_ in range (10) if _ % 2 == 0], [_for_ in range(20) if _ % 2 != 0])
