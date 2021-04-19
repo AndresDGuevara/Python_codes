ac1dc2e493e89da489604054049a337503b928d9
Andres Guevara <david.784@outlook.com>
2021-04-19 11:20:29 -0500

    adding a while loop

--- intermediate/unfinished/5of_hearts.py
+++ intermediate/unfinished/5of_hearts.py
@@ -18,3 +18,35 @@ def choose_black(num):
     print(num, 'of clubs')
   else:
     print(num, 'of spades')
+
+def run():
+  print("\n\tWelcome to 5 of hearts\n")
+  
+  num = int(input("\tEnter a number: "))
+  while True:
+    menu = """ Choose one of the options below
+    1- Draw Cards
+    2- Choose Red
+    3- Choose Black
+    4- Quit the game
+    Choose one of the options: 
+    """
+    try:
+      option = int(input(menu))
+    except ValueError:
+      print("Wrong choice!, try again")
+      continue
+
+    if option == 1:
+      draw_cards(num)
+    elif option == 2:
+      choose_red(num)
+    elif option == 3:
+      choose_black(num)
+    elif option == 4:
+      print("Thanks for playing")
+      break
+
+if __name__ == '__main__':
+  run()
+  
